2e56e68fd4274385ff641bd36193f657f2a5b27c
Dave Pearson <davep@davep.org>
2024-04-25 17:35:16 +0100

    :sparkles: Hang on to the raw text of the various forms of output
    This will come in handy for saving operations, etc.

--- natter/widgets/output.py
+++ natter/widgets/output.py
@@ -2,6 +2,7 @@
 
 ##############################################################################
 # Textual imports.
+from textual.await_complete import AwaitComplete
 from textual.widgets import Label, Markdown
 
 
@@ -21,6 +22,28 @@ class Agent(Markdown):
     }
     """
 
+    def update(self, markdown: str) -> AwaitComplete:
+        """Update the document with new Markdown.
+
+        Args:
+            markdown: A string containing Markdown.
+
+        Returns:
+            An optionally awaitable object. Await this to ensure that all
+            children have been mounted.
+        """
+        # Update the internal copy of the markdown; this is normally only
+        # used in the constructor to delay parsing of the document, but
+        # doing it here too means that it can be used to access the raw text
+        # later on.
+        self._markdown = markdown
+        return super().update(markdown)
+
+    @property
+    def raw_text(self) -> str:
+        """The raw text."""
+        return self._markdown
+
 
 ##############################################################################
 class User(Label):
@@ -33,6 +56,15 @@ class User(Label):
         padding: 1 2;
     }
     """
+
+    def __init__(self, output: str) -> None:
+        super().__init__(output)
+        self._raw_text = output
+
+    @property
+    def raw_text(self) -> str:
+        """The raw text."""
+        return self._raw_text
 
 
 ##############################################################################
