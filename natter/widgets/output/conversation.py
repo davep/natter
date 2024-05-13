825149eac44e0780eb093c1228d7cd9ae731c2fd
Dave Pearson <davep@davep.org>
2024-05-13 09:24:16 +0100

    :hammer: Move Interaction outwith of Conversation

--- natter/widgets/output/conversation.py
+++ natter/widgets/output/conversation.py
@@ -26,6 +26,64 @@ from .user import User
 
 
 ##############################################################################
+class Interaction:
+    """Context manager for an instance of interaction in the conversation."""
+
+    def __init__(self, conversation: Conversation, user_input: str) -> None:
+        """Initialise the interaction.
+
+        Args:
+            conversation: The conversation that this interaction is part of.
+            user_input: The input from the user starting the interaction.
+        """
+        self._conversation = conversation
+        self._user = User(user_input)
+        self._agent = Agent()
+        self._loading = LoadingIndicator()
+
+    async def __aenter__(self) -> Self:
+        """Mount the widgets needed for the interaction.
+
+        Mounts the user's input, the space for the agent's reply, and
+        also the loading indicator.
+        """
+        await self._conversation.mount_all([self._user, self._agent, self._loading])
+        self._conversation.scroll_end()
+        return self
+
+    async def update_response(self, response: str) -> None:
+        """Update the interaction with the agent's response.
+
+        Args:
+            response: The response to update with.
+        """
+        await self._agent.update(self._agent.raw_text + response)
+        self._conversation.scroll_end()
+
+    async def abandon(self, reason: str) -> None:
+        """Abandon the interaction.
+
+        Args:
+            reason: The reason to abandon the interaction.
+        """
+        await self._agent.remove()
+        await self._conversation.mount(Error(reason))
+
+    async def __aexit__(
+        self,
+        exc_type: type[BaseException] | None,
+        exc_val: BaseException | None,
+        exc_traceback: TracebackType | None,
+    ) -> None:
+        """Clean up at the end of the interaction.
+
+        Removes the loading indicator.
+        """
+        del exc_type, exc_val, exc_traceback
+        await self._loading.remove()
+
+
+##############################################################################
 class Conversation(VerticalScroll, can_focus=False):
     """Container for displaying a whole conversation."""
 
@@ -58,62 +116,6 @@ class Conversation(VerticalScroll, can_focus=False):
             ]
         )
 
-    class Interaction:
-        """Context manager for an instance of interaction in the conversation."""
-
-        def __init__(self, conversation: Conversation, user_input: str) -> None:
-            """Initialise the interaction.
-
-            Args:
-                conversation: The conversation that this interaction is part of.
-                user_input: The input from the user starting the interaction.
-            """
-            self._conversation = conversation
-            self._user = User(user_input)
-            self._agent = Agent()
-            self._loading = LoadingIndicator()
-
-        async def __aenter__(self) -> Self:
-            """Mount the widgets needed for the interaction.
-
-            Mounts the user's input, the space for the agent's reply, and
-            also the loading indicator.
-            """
-            await self._conversation.mount_all([self._user, self._agent, self._loading])
-            self._conversation.scroll_end()
-            return self
-
-        async def update_response(self, response: str) -> None:
-            """Update the interaction with the agent's response.
-
-            Args:
-                response: The response to update with.
-            """
-            await self._agent.update(self._agent.raw_text + response)
-            self._conversation.scroll_end()
-
-        async def abandon(self, reason: str) -> None:
-            """Abandon the interaction.
-
-            Args:
-                reason: The reason to abandon the interaction.
-            """
-            await self._agent.remove()
-            await self._conversation.mount(Error(reason))
-
-        async def __aexit__(
-            self,
-            exc_type: type[BaseException] | None,
-            exc_val: BaseException | None,
-            exc_traceback: TracebackType | None,
-        ) -> None:
-            """Clean up at the end of the interaction.
-
-            Removes the loading indicator.
-            """
-            del exc_type, exc_val, exc_traceback
-            await self._loading.remove()
-
     def interaction(self, user_input: str) -> Interaction:
         """Create an interaction within the conversation.
 
@@ -123,7 +125,7 @@ class Conversation(VerticalScroll, can_focus=False):
         Returns:
             An `Interaction` context manager.
         """
-        return self.Interaction(self, user_input)
+        return Interaction(self, user_input)
 
 
 ### conversation.py ends here
